14945f42bdcc0df334605ec79e912587385bccd3
Jason Tran <jasontran2022@gmail.com>
2022-04-26 11:22:02 -0400

    finish data-viz project

--- projects/data-viz/index.js
+++ projects/data-viz/index.js
@@ -10,41 +10,27 @@ const studySessions = [
   { date: '4/13', day: 'Wednesday', totalMins: 120, left: 950, top: 550, cis545: 0, cis188: 120, dsgn234: 0, engl135: 0, ealc064: 0 },
 ]
 
+// an array of color objects
 const colorArray = [
-  { code: 'CIS 545', name: 'Data Science', color: '#EC9F05' },
+  { code: 'CIS 545', name: 'Data Science', color: '#F9D976' },
   { code: 'CIS 188', name: 'DevOps', color: '#FFDD00' },
   { code: 'DSGN 234', name: 'Art of the Web', color: '#FB7BA2' },
   { code: 'ENGL 135', name: 'English', color: '#FBA8A4' },
   { code: 'EALC 064', name: 'Sushi and Ramen', color: '#E899DC' },
 ]
 
+// a variable to indicate if the page is on the study session display
+let isOnStudySession = true
+
 // get the background container
 const container = document.querySelector(".background")
-let i = 0
 
-// create a canvas for the study session shapes
-// studySessions.forEach(studySession => {
-//   const newItem = document.createElement("div")
-//   const dateDiv = container.appendChild(newItem)
-//   i++;
-//   newItem.classList.add("date")
-//   newItem.classList.add("date" + [i])
+// create the legend div for future use
+const legend = document.createElement("div")
 
-//   const { date, day, top, left, totalMins, cis545, cis188, dsgn234, engl135, ealc064 } = studySession
-//   dateDiv.innerHTML = `${date}`
-//   dateDiv.style.height = `${totalMins}px`
-//   dateDiv.style.width = `${totalMins}px`
-//   dateDiv.style.position = 'absolute'
-//   dateDiv.style.top = `${top}px`
-//   dateDiv.style.left = `${left}px`
-
-//   dateDiv.addEventListener("mouseover", () => {
-//     dateDiv.innerHTML = `${day} \n CirTotal Mins: ${totalMins} \n Data Science: ${cis545} \n DevOps: ${cis188} \n Art of the Web: ${dsgn234} \n English: ${engl135} \n Sushi and Ramen: ${ealc064}`
-//   });
-//   dateDiv.addEventListener("mouseleave", () => {
-//     dateDiv.innerHTML = `${date}`
-//   });
-// })
+// display the study sessions
+const dates = document.createElement("div")
+displayStudySessions()
 
 // create a button to toggle between div elements
 const button = document.createElement("div")
@@ -52,57 +38,103 @@ const buttonDiv = container.appendChild(button)
 buttonDiv.classList.add("button")
 buttonDiv.innerHTML = 'Learn More!'
 
-// create a legend section
-const legend = document.createElement("div")
-const legendDiv = container.appendChild(legend)
-legendDiv.classList.add("legend")
-legendDiv.innerHTML = 'Legend'
-
-const shapes = document.createElement("div")
-const shapesDiv = legend.appendChild(shapes)
-shapesDiv.classList.add("shapes")
-
-// create a weekday div
-const square = document.createElement("div")
-const squareDiv = shapes.appendChild(square)
-squareDiv.classList.add("shape")
-squareDiv.innerHTML = 'Weekday'
-
-// create a weekend div
-const circle = document.createElement("div")
-const circleDiv = shapes.appendChild(circle)
-circleDiv.classList.add("shape")
-circleDiv.classList.add("weekend")
-circleDiv.innerHTML = 'Weekend'
-
-const shape = document.createElement("div")
-const shapeDiv = shapes.appendChild(shape)
-shapeDiv.classList.add("shape")
-shapeDiv.classList.add("weekend")
-shapeDiv.classList.add("size")
-shapeDiv.innerHTML = 'Size = Total Mins'
-
-// create a color code section
-const colors = document.createElement("div")
-const colorsDiv = legend.appendChild(colors)
-colorsDiv.classList.add("colors")
-
-// create individual color block
-colorArray.forEach(colorItem => {
-  const newItem = document.createElement("div")
-  const colorDiv = colorsDiv.appendChild(newItem)
-  i++;
-  newItem.classList.add("color")
-  newItem.classList.add("color" + [i])
-
-  const { name, color, code } = colorItem
-  colorDiv.innerHTML = `${name}`
-  colorDiv.style.backgroundColor = `${color}`
-
-  colorDiv.addEventListener("mouseover", () => {
-    colorDiv.innerHTML = `${code}`
-  });
-  colorDiv.addEventListener("mouseleave", () => {
-    colorDiv.innerHTML = `${name}`
-  });
+// add event listeners to button
+buttonDiv.addEventListener("click", () => {
+  if (isOnStudySession) {
+    container.removeChild(dates)
+    container.appendChild(legend)
+    displayLegend()
+    buttonDiv.innerHTML = 'Return'
+    isOnStudySession = false
+  } else {
+    container.removeChild(legend)
+    container.appendChild(dates)
+    buttonDiv.innerHTML = 'Learn More!'
+    isOnStudySession = true
+  }
 })
+
+// create a canvas for the study session shapes
+function displayStudySessions() {
+  let i = 0
+  const datesDiv = container.appendChild(dates)
+  studySessions.forEach(studySession => {
+    const newItem = document.createElement("div")
+    const dateDiv = datesDiv.appendChild(newItem)
+    i++;
+    newItem.classList.add("date")
+    newItem.classList.add("date" + [i])
+
+    const { date, day, top, left, totalMins, cis545, cis188, dsgn234, engl135, ealc064 } = studySession
+    dateDiv.innerHTML = `${date}`
+    dateDiv.style.height = `${totalMins}px`
+    dateDiv.style.width = `${totalMins}px`
+    dateDiv.style.position = 'absolute'
+    dateDiv.style.top = `${top}px`
+    dateDiv.style.left = `${left}px`
+
+    dateDiv.addEventListener("mouseover", () => {
+      dateDiv.innerHTML = `${day} \n Total Mins: ${totalMins} \n Data Science: ${cis545} \n DevOps: ${cis188} \n Art of the Web: ${dsgn234} \n English: ${engl135} \n Sushi and Ramen: ${ealc064}`
+    });
+    dateDiv.addEventListener("mouseleave", () => {
+      dateDiv.innerHTML = `${date}`
+    });
+  })
+}
+
+function displayLegend() {
+  // create a legend section
+  const legendDiv = container.appendChild(legend)
+  legendDiv.classList.add("legend")
+  legendDiv.innerHTML = 'Legend'
+
+  const shapes = document.createElement("div")
+  const shapesDiv = legend.appendChild(shapes)
+  shapesDiv.classList.add("shapes")
+
+  // create a weekday div
+  const square = document.createElement("div")
+  const squareDiv = shapes.appendChild(square)
+  squareDiv.classList.add("shape")
+  squareDiv.innerHTML = 'Weekday'
+
+  // create a weekend div
+  const circle = document.createElement("div")
+  const circleDiv = shapes.appendChild(circle)
+  circleDiv.classList.add("shape")
+  circleDiv.classList.add("weekend")
+  circleDiv.innerHTML = 'Weekend'
+
+  const shape = document.createElement("div")
+  const shapeDiv = shapes.appendChild(shape)
+  shapeDiv.classList.add("shape")
+  shapeDiv.classList.add("weekend")
+  shapeDiv.classList.add("size")
+  shapeDiv.innerHTML = 'Size = Total Mins'
+
+  // create a color code section
+  const colors = document.createElement("div")
+  const colorsDiv = legend.appendChild(colors)
+  colorsDiv.classList.add("colors")
+
+  // create individual color block
+  let i = 0
+  colorArray.forEach(colorItem => {
+    const newItem = document.createElement("div")
+    const colorDiv = colorsDiv.appendChild(newItem)
+    i++;
+    newItem.classList.add("color")
+    newItem.classList.add("color" + [i])
+
+    const { name, color, code } = colorItem
+    colorDiv.innerHTML = `${name}`
+    colorDiv.style.backgroundColor = `${color}`
+
+    colorDiv.addEventListener("mouseover", () => {
+      colorDiv.innerHTML = `${color}`
+    });
+    colorDiv.addEventListener("mouseleave", () => {
+      colorDiv.innerHTML = `${name}`
+    });
+  })
+}
